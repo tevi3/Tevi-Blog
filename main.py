f45342f31bbfd5ede7912cee1f8406e8f4855a98
tevi <tevi3@naver.com>
2023-07-06 13:59:33 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -74,7 +74,8 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 def admin_only(func):
